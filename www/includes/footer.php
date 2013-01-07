a1029b208967e758ed53dfe88acd2865ee2a25a6
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2013-01-07 15:24:56 +0100

    Kickstart

---
+++ www/includes/footer.php
@@ -0,0 +1,30 @@
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="span3">
+					[WORK IN PROGRESS]
+				
+				</div>
+				<div class="span3">
+					Navigation
+					<li>
+						<a href="x.php">
+							x
+						</a>
+					</li>
+				</div>
+				<div class="span3">
+					Thanks to...
+				</div>
+				<div class="span3">
+					<div style="text-align:right">
+						<a href="http://medialab.sciences-po.fr"><img src="res/logosp_fonce.png"/></a>
+						<br/>
+						Developed by Mathieu Jacomy
+						<br/>
+						at the <a href="http://medialab.sciences-po.fr">Sciences-Po Medialab</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
---
+++ www/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/libs/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">This tool</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <!--<li class="active"><a href="#">Gaga</a></li>-->
+                            <li><a href="#x">xxx</a></li>
+                            <!--
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li class="nav-header">Nav header</li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                            -->
+                        </ul>
+
+                       <div class="minilogo pull-right">
+                            <a href="http://medialab.sciences-po.fr"><img src="res/mini-tools.png"/></a>
+                            <a href="http://tools.medialab.sciences-po.fr"><img src="res/mini-sp.png"/></a>
+                        </div>
+
+                        <!-- <form class="navbar-form pull-right">
+                            <input class="span2" type="text" placeholder="Email">
+                            <input class="span2" type="password" placeholder="Password">
+                            <button type="submit" class="btn">Sign in</button>
+                        </form>
+                         -->
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="container">
+
+            <!-- Main hero unit for a primary marketing message or call to action -->
+            <div class="splash-unit row">
+                <div class="span7">
+                    <div class="image">
+                        <a href="index.php"><img src="res/header.png"/></a>
+                    </div>
+                    <div class="title">
+                        some médialab tool
+                    </div>
+                </div>
+                <div class="span5">
+                    <div class="abstract">
+                        <p><strong>Catchline.</strong> Stupid simple description.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="span4">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View details &raquo;</a></p>
+                </div>
+                <div class="span4">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View details &raquo;</a></p>
+               </div>
+                <div class="span4">
+                    <h2>Heading</h2>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <p><a class="btn" href="#">View details &raquo;</a></p>
+                </div>
+            </div>
+        </div>
+
+        <?php include("includes/footer.php"); ?>
+
+        <?php include("includes/footcontent.php"); ?>
+
+    </body>
+</html>
