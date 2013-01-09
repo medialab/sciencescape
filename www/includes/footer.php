d2744602597b52a1d1ad4449f544f682ffdc0d92
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2013-01-09 15:39:21 +0100

    Polish

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -1,28 +1,32 @@
 	<footer>
 		<div class="container">
 			<div class="row">
+				<div class="span12">
+					<hr/>
+				</div>
 				<div class="span3">
-					[WORK IN PROGRESS]
+					<!-- [WORK IN PROGRESS] -->
 				
 				</div>
 				<div class="span3">
-					Navigation
+					<!-- Navigation
 					<li>
 						<a href="x.php">
 							x
 						</a>
-					</li>
+					</li> -->
 				</div>
 				<div class="span3">
-					Thanks to...
+					
 				</div>
 				<div class="span3">
 					<div style="text-align:right">
 						<a href="http://medialab.sciences-po.fr"><img src="res/logosp_fonce.png"/></a>
-						<br/>
-						Developed by Mathieu Jacomy
-						<br/>
-						at the <a href="http://medialab.sciences-po.fr">Sciences-Po Medialab</a>
+						<p class="muted">
+							Developed by Mathieu Jacomy
+							<br/>
+							at the <a href="http://medialab.sciences-po.fr">Sciences-Po Medialab</a>
+						</p>
 					</div>
 				</div>
 			</div>
