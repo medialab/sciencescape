07da1f62441fa88ae0d5e8ae02b2a09765d00f10
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2013-10-11 19:05:11 +0200

    various things T_T'
    recovery from BSOD during commit...

--- js/_page_scopus_papersvolume_evolution.js
+++ js/_page_scopus_papersvolume_evolution.js
@@ -32,9 +32,15 @@ domino.settings({
         ],services: [
         ],hacks:[
             {
-                // Needed to be declared:
+                // Need to be declared
                 triggers:[
-                    'extraction_success'
+                    'loading_started'
+                    ,'loading_completed'
+                    ,'parsing_init'
+                    ,'parsing_pending'
+                    ,'parsing_success'
+                    ,'parsing_fail'
+                    ,'extraction_success'
                     ,'extraction_pending'
                     ,'extraction_fail'
                 ]
@@ -43,20 +49,20 @@ domino.settings({
                 // (NB: init the progress bar will trigger pending, and pending will trigger the parsing)
                 triggers:['loading_completed']
                 ,method: function(){
-                    D.dispatchEvent('parsing_init')
+                    this.dispatchEvent('parsing_init')
                 }
             },{
                 // Init extraction progress bar when the table is parsed
                 triggers:['dataTable_updated']
                 ,method: function(){
-                    D.dispatchEvent('extraction_init')
+                    this.dispatchEvent('extraction_init')
                 }
             },{
                 // Redraw when 'normalize' is enabled or disabled
                 triggers:['normalize_updated']
                 ,method: function(){
-                    if(D.get('papersVolume') !== undefined)
-                        buildAndShow()
+                    if(this.get('papersVolume') !== undefined)
+                        buildAndShow(this)
                 }
             }
         ]
@@ -68,12 +74,8 @@ domino.settings({
     D.addModule(function(){
         domino.module.call(this)
 
-        this.triggers.events['loadingProgress_updated'] = function(d) {
-            // console.log('Loading progress', D.get('loadingProgress'))
-        }
-
-        this.triggers.events['networkJson_updated'] = function(d) {
-            console.log('Network: ',D.get('networkJson'))
+        this.triggers.events['networkJson_updated'] = function(provider, e) {
+            console.log('Network: ', provider.get('networkJson'))
         }
     })
 
@@ -95,8 +97,8 @@ domino.settings({
         })
 
         
-        this.triggers.events['inputFile_updated'] = function(){
-            var files = D.get('inputFile')
+        this.triggers.events['inputFile_updated'] = function(provider, e){
+            var files = provider.get('inputFile')
             if( files !== undefined && files.length >0 ){
                 container.find('div.input').hide()
                 container.find('div.progress').show()
@@ -104,20 +106,20 @@ domino.settings({
                 bar.css('width', '0%')
                 
                 var fileLoader = new FileLoader()
-                D.dispatchEvent('update_inputFileUploader', {
+                _self.dispatchEvent('update_inputFileUploader', {
                     inputFileUploader: fileLoader
                 })
                 fileLoader.read(files, {
                     onloadstart: function(evt){
-                        D.dispatchEvent('loading_started')
+                        _self.dispatchEvent('loading_started')
                     },
                     onload: function(evt){
-                        D.dispatchEvent('loading_completed')
+                        _self.dispatchEvent('loading_completed')
                     },
                     onprogress: function(evt){
                         // evt is an ProgressEvent
                         if (evt.lengthComputable) {
-                            D.dispatchEvent('update_loadingProgress', {
+                            _self.dispatchEvent('update_loadingProgress', {
                                 loadingProgress: Math.round((evt.loaded / evt.total) * 100)
                             })
                         }
@@ -126,8 +128,8 @@ domino.settings({
             }
         }
 
-        this.triggers.events['loadingProgress_updated'] = function(){
-            var percentLoaded = +D.get('loadingProgress')
+        this.triggers.events['loadingProgress_updated'] = function(provider, e){
+            var percentLoaded = +provider.get('loadingProgress')
             // Increase the progress bar length.
             if (percentLoaded < 100) {
                 var bar = container.find('div.progress .bar')
@@ -155,32 +157,32 @@ domino.settings({
     var ProgressBar = function(options, d) {
         domino.module.call(this)
 
-        var self = this
+        var _self = this
             ,o = options || {}
             ,el = o['element'] || $('<div/>')
 
         el.html('<div style="height: 25px;"><div class="progress" style="display: none"><div class="bar" style="width: 0%;"></div></div></div>')
 
-        self.triggers.events[o['taskname']+'_init'] = function(){
+        this.triggers.events[o['taskname']+'_init'] = function(provider, e){
             el.find('div.progress').show()
             el.find('div.progress').addClass('progress-striped')
             el.find('div.progress').addClass('active')
             el.find('div.progress div.bar').css('width', '100%')
             el.find('div.progress div.bar').text(o['taskname']+'...')
-            D.dispatchEvent(o['taskname']+'_pending', {})
+            _self.dispatchEvent(o['taskname']+'_pending', {})
         }
 
-        self.triggers.events[o['taskname']+'_pending'] = function(){
+        this.triggers.events[o['taskname']+'_pending'] = function(){
             el.find('div.progress').removeClass('progress-striped')
             el.find('div.progress').removeClass('active')
         }
 
-        self.triggers.events[o['taskname']+'_success'] = function(){
+        this.triggers.events[o['taskname']+'_success'] = function(){
             el.find('div.progress div.bar').addClass('bar-success')
             el.find('div.progress div.bar').text(o['taskname']+' successful')
         }
 
-        self.triggers.events[o['taskname']+'_fail'] = function(){
+        this.triggers.events[o['taskname']+'_fail'] = function(){
             el.find('div.progress div.bar').addClass('bar-danger')
             el.find('div.progress div.bar').text(o['taskname']+' failed')
         }
@@ -204,19 +206,21 @@ domino.settings({
     D.addModule(function(){
         domino.module.call(this)
 
-        this.triggers.events['parsing_pending'] = function(){
-            var fileLoader = D.get('inputFileUploader')
-                ,wok_data = build_wosDoiLinks(fileLoader.reader.result)
-            if(wok_data){
+        var _self = this
+
+        this.triggers.events['parsing_pending'] = function(provider, e){
+            var fileLoader = provider.get('inputFileUploader')
+                ,data = parse_CSV_rows(fileLoader.reader.result)
+            if(data){
                 setTimeout(function(){
-                    D.dispatchEvent('update_dataTable', {
-                        'dataTable': wok_data
+                    _self.dispatchEvent('update_dataTable', {
+                        'dataTable': data
                     })
                 }, 200)
 
-                D.dispatchEvent('parsing_success', {})
+                _self.dispatchEvent('parsing_success', {})
             } else {
-                D.dispatchEvent('parsing_fail', {})
+                _self.dispatchEvent('parsing_fail', {})
             }
         }
     })
@@ -265,11 +269,11 @@ domino.settings({
     
 
     //// Data processing
-    function buildAndShow(){
+    function buildAndShow(provider){
         $('#timeline').html('')
 
-        var years = D.get('papersVolume')
-            ,table = D.get('dataTable')
+        var years = provider.get('papersVolume')
+            ,table = provider.get('dataTable')
             ,yearColId = -1
         table[0].forEach(function(txt, i){
             if(txt == 'PY (Year Published)' || txt == 'PY')
@@ -361,120 +365,9 @@ domino.settings({
             .call(xAxis)
 
     }
-    function build_wosDoiLinks(wos){
-        if ( wos.substring(0,2) != "FN" ){
-            return build_DoiLinks(wos, d3.csv.parseRows, 'CR', 'DOI_CITED')
-        } else {
-            return convert_wos_to_CSV(wos, true)
-        }
-    }
-
-    function build_DoiLinks(csv, rowsParser, column, doi_column_name){
-        var lines = rowsParser(csv)
-        var headline = lines.shift()
-        var CR_index = -1;  // Index containing the references
-        headline.forEach(function(h,i){
-            if(h == column){
-                CR_index = i
-            }
-        })
-        var csvRows = [headline]
-        lines.forEach(function(row){
-            if(CR_index>=0 && CR_index < row.length){
-                // Extract DOI reference of the cited paper if applicable
-                var doi_refs = d3.merge(row[CR_index]
-                    .split(";")
-                    .map(function(ref){
-                        return ref.split(",").filter(function(d){
-                            return d.match(/ +DOI[ :]+.*/gi)
-                        })
-                    })).map(function(doi){
-                        //return doi.trim().split(/[ :]/)[1] || ""
-                        var r = / +DOI[ :]+(.*)/gi
-                        return r.exec(doi)[1] || ''
-                    }).filter(function(doi){
-                        return doi.trim() != ""
-                    })
-                row.unshift(doi_refs.join("; "))
-            } else {
-                row.unshift("")
-            }
-            csvRows.push(row)
-        })
-        
-        headline.unshift(doi_column_name)
-        return csvRows
-    }
-
-    function convert_wos_to_CSV(wos, extractDOI){
-        var data = [];
-        var currentFieldTag = "";
-        var currentItem = {};
-        var lines = wos.split("\n");
-        if(lines.shift().substring(0,2) == "FN"){
-            lines.forEach(function(line){
-                var candidateFieldTag = line.substring(0,2);
-                if(candidateFieldTag != "  "){
-                    currentFieldTag = candidateFieldTag;
-                }
-                
-                if(currentFieldTag == "ER"){
-                    data.push(currentItem);
-                    currentItem = new Object();
-                } else {
-                    if(currentItem[currentFieldTag]){
-                        if(currentFieldTag=="AU"
-                            || currentFieldTag=="AF"
-                            || currentFieldTag=="C1"
-                            || currentFieldTag=="CR"
-                        ){
-                            currentItem[currentFieldTag] += "|" + line.substring(3);
-                        } else {
-                            currentItem[currentFieldTag] += " " + line.substring(3);
-                        }
-                        
-                        // Extract citations
-                        if(currentFieldTag=="CR" && extractDOI){
-                            // Extract DOI reference of the cited paper if applicable
-                            var doi_refs = line.substring(3).match(/DOI.*/gi);
-                            if(doi_refs && doi_refs.length>0){
-                                if(currentItem["DOI_CITED"]){
-                                    currentItem["DOI_CITED"] += "|" + doi_refs[0].split(" ")[1];
-                                } else {
-                                    currentItem["DOI_CITED"] = doi_refs[0].split(" ")[1];
-                                }
-                            }
-                        }
-                    } else {
-                        currentItem[currentFieldTag] = line.substring(3);
-                    }
-                }
-            });
-        } else {
-            alert("There is a problem with the WOS file\n(No FileName fieldtag)");
-        }
-
-        // At this point, data is a list of objects.
-        // Convert it to CSV
-
-        var headers = []
-        data.forEach(function(item){
-            for(i in item){
-                if(!headers.some(function(x){return x==i;})){
-                    headers.push(i)
-                }
-            }
-        });
-
-        var csvRows = []
-        csvRows.push(headers)
-
-        data.forEach(function(item){
-            csvRows.push(headers.map(function(header){
-                return item[header] || '';
-            }))
-        })
-        return csvRows
+    function parse_CSV_rows(csv){
+        var rowsParser = d3.csv.parseRows
+        return rowsParser(csv)
     }
 
     // Utilities
---
+++ scopus_keywords_evolution.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>ScienceScape</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+<?php include('includes/config_management.php') ?>
+<?php include('includes/codetop.php') ?>
+        <style>
+
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+
+            svg{
+                margin-bottom: 4px;
+            }
+
+            .grid line {
+              stroke: #fff;
+            }
+
+            .grid line.minor {
+              stroke-width: .5px;
+            }
+
+            .grid text {
+              display: none;
+            }
+
+            path.domain {
+              display: none;
+            }
+
+            #timelines{
+                margin-top: 30px;
+            }
+        </style>
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+<?php include('includes/header.php') ?>
+
+
+
+
+        <div class="container">
+
+            <!-- Main hero unit for a primary marketing message or call to action -->
+            <div class="splash-unit row">
+                <div class="span7">
+                    <div class="image">
+                        <a href="index.php"><img src="res/header.png"/></a>
+                    </div>
+                    <div class="title">
+                        ScienceScape
+                    </div>
+                </div>
+                <div class="span5">
+                    <div class="abstract">
+                        <p><strong>Helpers for scientometrics.</strong> Convert files, get networks, visualize stuff from Scopus or Web of Science.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="span12">
+                    <h2>Keywords Evolution</h2>
+                    <p class="text-info">
+                        Upload a Scopus file containing author keywords and year of publication,
+                        and visualize the evolution of most important keywords (&gt;10 papers in the file).
+                        The normalization divides by the total count of papers each year.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="span4">
+                    <div id="extract"><span class="muted">file uploader</span></div>
+                </div>
+                <div class="span4">
+                    <div id="parsing"><span class="muted">parsing</span></div>
+                </div>
+                <div class="span4">
+                    <div id="extraction"><span class="muted">extraction</span></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="span4">
+                    <label class="checkbox">
+                        <input id="normalize" type="checkbox"> Normalize
+                    </label>
+                </div>
+                <div class="span4">
+                </div>
+                <div class="span4">
+                    <div id="download"><span class="muted">download</span></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="span12">
+                    <div id="timelines"></div>
+                </div>
+            </div>
+        </div>
+
+        <?php include("includes/footer.php"); ?>
+
+        <?php include("includes/codebottom.php"); ?>
+
+        <script src="js/libs/horizon.js"></script>
+        <script src="js/_page_scopus_keywords_evolution.js"></script>
+
+        <script>
+            // document.getElementById('scopusextract_input').addEventListener('change', fileLoader.handleFileSelect, false);
+        </script>
+    </body>
+</html>
